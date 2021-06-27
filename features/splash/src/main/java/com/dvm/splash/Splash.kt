eee2c3d081a0a59cef84ec954810459e13160db5
Dmitry <mdimail@list.ru>
2021-06-27 15:30:57 +0400

    Fix splash landscape image size.

--- features/splash/src/main/java/com/dvm/splash/Splash.kt
+++ features/splash/src/main/java/com/dvm/splash/Splash.kt
@@ -1,12 +1,15 @@
 package com.dvm.splash
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieAnimationSpec
 import com.airbnb.lottie.compose.rememberLottieAnimationState
@@ -17,13 +20,23 @@ internal fun Splash() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
+        val configuration = LocalConfiguration.current
+
+        val rows = when (configuration.orientation) {
+            Configuration.ORIENTATION_LANDSCAPE -> 5
+            else -> 3
+        }
+
         val animationSpec = remember { LottieAnimationSpec.RawRes(R.raw.splash) }
         val animationState =
             rememberLottieAnimationState(autoPlay = true, repeatCount = Integer.MAX_VALUE)
         LottieAnimation(
             spec = animationSpec,
             animationState = animationState,
-            modifier = Modifier.fillMaxWidth()
+            modifier = when (configuration.orientation) {
+                Configuration.ORIENTATION_LANDSCAPE -> Modifier.fillMaxHeight()
+                else -> Modifier.fillMaxWidth()
+            }
         )
     }
 }
