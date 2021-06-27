49fefebb727884fa3f68cd56dc9af8df47e5b82b
Dmitry <mdimail@list.ru>
2021-06-27 13:55:47 +0400

    Add comment for app menu feature api.

--- features/appmenu-api/src/main/java/com/dvm/appmenu_api/Drawer.kt
+++ features/appmenu-api/src/main/java/com/dvm/appmenu_api/Drawer.kt
@@ -7,6 +7,11 @@ import androidx.compose.runtime.Composable
 import com.dvm.appmenu.AppDrawer
 import com.dvm.utils.DrawerItem
 
+// It's not the way how we usually handle features api.
+// Usually we have interface here.
+// But the main point is to prevent feature dependency on another feature.
+// In this scenario we don't need to use some class from app menu feature,
+// but just to invoke a composable function.
 @Composable
 fun Drawer(
     drawerState: DrawerState = rememberDrawerState(DrawerValue.Closed),
