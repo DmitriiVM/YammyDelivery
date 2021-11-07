2ca0f2b042686bc943dc1bdf837a6eb43d256909
Dmitry <mdimail@list.ru>
2021-11-07 17:28:14 +0400

    Update gradle plugin and AGP.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.1.0-alpha08")
+    implementation("com.android.tools.build:gradle:7.2.0-alpha03")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
     implementation("com.google.gms:google-services:4.3.10")
     implementation("com.squareup.sqldelight:gradle-plugin:1.5.0")
