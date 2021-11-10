04cd1adf481b51718e7443609314d97519d75a68
Dmitry <mdimail@list.ru>
2021-11-10 18:29:14 +0400

    Set plugins via versions catalog.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,10 +8,10 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.2.0-alpha03")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
-    implementation("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
-    implementation("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
-    implementation("com.google.gms:google-services:4.3.10")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.18.1")
+    implementation(lib.plugin.androidGradle)
+    implementation(lib.plugin.kotlinGradle)
+    implementation(lib.plugin.hilt)
+    implementation(lib.plugin.safeargs)
+    implementation(lib.plugin.google.services)
+    implementation(lib.plugin.detekt)
 }
